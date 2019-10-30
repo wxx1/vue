27d6508c5656603183f94f512cfa8317295fd211
wxx <1079565530@qq.con>
2019-10-30 18:08:26 +0800

    新加购物车功能

---
+++ shopping/src/views/cart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cart">
+    <div class="cart-header">
+      <div class="cart-header-title">购物清单</div>
+      <div class="cart-header-main">
+        <div class="cart-info">商品清单</div>
+        <div class="cart-price">单价</div>
+        <div class="cart-price">数量</div>
+        <div class="cart-price">小计</div>
+        <div class="cart-price">删除</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cart {
+  margin: 32px;
+  padding: 32px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  background-color: #fff;
+  overflow: hidden;
+}
+</style>
